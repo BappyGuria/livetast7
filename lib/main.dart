c18b2ac0d1e63e353ad913ef5310f763bd4f9e51
BappyGuria <guriabappye8@gmail.com>
2023-09-24 23:35:41 +0600

    Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,97 @@
 import 'package:flutter/material.dart';
 
-import 'MyHomePage.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(),
+      home: CounterScreen(),
     );
   }
 }
 
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+
+  void _incrementCount() {
+    setState(() {
+      count++;
+      if (count >= 5) {
+        _showDialog();
+      }
+    });
+  }
+
+  void _decrementCount() {
+    setState(() {
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Button pressed $count times"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Count: ",
+              style: TextStyle(fontSize: 24),
+            ),
+            Text('${count}',style: TextStyle(
+              fontSize: 45
+            ),),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _decrementCount,
+                  child: Text("-",style: TextStyle(fontSize: 25)),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _incrementCount,
+                  child: Text("+",style: TextStyle(fontSize: 25),),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
